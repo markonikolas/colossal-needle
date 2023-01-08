95b4d4f9dac0972c2806a1e61c6702c7ab7f33c4
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-08 12:54:23 -0600

    Fix TypeOrm refused connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import configuration from './config/configuration';
+import configuration, { databaseConfig } from './config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+    }),
+    TypeOrmModule.forRoot({
+      ...databaseConfig(),
     }),
   ],
   controllers: [AppController],
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,16 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 export default () => ({
-  port: process.env.APP_PORT || 5000,
+  port: parseInt(process.env.APP_PORT || '5000', 10),
+});
+
+export const databaseConfig = (): TypeOrmModuleOptions => ({
+  type: 'mariadb',
+  host: process.env.MYSQL_HOST,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  port: parseInt(process.env.MYSQL_TCP_PORT || '3306', 10),
+  entities: [],
+  synchronize: true,
 });
