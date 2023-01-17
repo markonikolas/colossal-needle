a7166dd69b6b81454da5aef5757f9adb542068aa
Peter Pumpkineater <n1-dev+pumpkineater6996@simplelogin.co>
2023-01-16 22:16:53 -0500

    Add temp route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/availability')
+  getAvailability(): any {
+    return { message: 'Operational', code: 200 };
+  }
 }
